63d53ff028dc473b174174f8dc4a0f40b16c2489
toni moragues <tmoragues@gmail.com>
2020-11-10 12:27:15 +0100

    index amb bdd

--- public/index.php
+++ public/index.php
@@ -1,3 +1,17 @@
+<?php
+$servername = "localhost";
+$username = "admin";
+$password = "manacor";
+$dbname = "botiga";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,10 +24,30 @@
 <body>
 <div class="container">
   <div class="row">
+<?php
+
+$sql = "SELECT codi, nom, preu, descripcio FROM productes";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+
+?>
+
     <div class="col-sm-3">
-      <img src="img/camiseta.jpg" alt="" class="img-fluid">
-      <h4 class="text-center">NOM PRODUCTE</h4>
+      <img src="img/<?php echo $row["codi"]; ?>.jpg" alt="" class="img-fluid">
+      <h4 class="text-center"><?php echo $row["nom"]; ?></h4>
     </div>
+
+<?php
+}
+} else {
+echo "0 results";
+}
+$conn->close();
+?>
+
   </div>
 </div>
 </body>
