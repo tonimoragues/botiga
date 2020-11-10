9c7e088c8cce23f5b82a4b5cb22687bd422ab9c0
toni moragues <tmoragues@gmail.com>
2020-11-10 11:58:12 +0100

    fitxa

---
+++ public/fitxa.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6"><img src="img/camiseta.jpg" alt="" class="img-fluid"></div>
+      <div class="col-sm-6">
+        <div class="row">
+          <div class="col-sm-11"><h2>NOM PRODUCTE</h2></div>
+          <div class="col-sm-11"><h4>40€</h4></div>
+          <div class="col-sm-11"><button class="btn btn-primary">Afegir</button></div>
+          <div class="col-sm-11"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente qui aperiam exercitationem nam aliquid excepturi vitae doloremque quae placeat culpa, laboriosam error tempora sequi repellat voluptate aut. Nihil illo fugit, tempore nemo cumque praesentium. Laborum nisi nihil ratione at, consequatur magni, voluptatem, nostrum ad rerum quibusdam ducimus quia libero possimus.</p></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
