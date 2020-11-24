7953bf29ba2d0f5a1a8f31e668b6d3d1d80f3e2c
toni moragues <tmoragues@gmail.com>
2020-11-24 14:38:54 +0100

    19

--- public/capsalera.php
+++ public/capsalera.php
@@ -7,7 +7,7 @@
 <div class="collapse navbar-collapse" id="navbarSupportedContent">
   <ul class="navbar-nav mr-auto">
     <li class="nav-item active">
-      <a class="nav-link" href="changelang.php?lang=cat">CAT</a>
+      <a class="nav-link" <?php if ($lag=='cat') {echo 'active';} ?> href="changelang.php?lang=cat">CAT</a>
     </li>
     <li class="nav-item">
       <a class="nav-link" href="changelang.php?lang=es">ES</a>
