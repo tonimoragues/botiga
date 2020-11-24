49e4f37eb0f1946ed7d391e007c5ebb89ea99a45
toni moragues <tmoragues@gmail.com>
2020-11-24 12:27:18 +0100

    4

--- public/capsalera.php
+++ public/capsalera.php
@@ -1,5 +1,13 @@
-  <div class="jumbotron jumbotron-fluid">
+  <div class="jumbotron jumbotron-fluid p-3">
     <div class="container">
       <h1 class="display-4">Botiga</h1>
+      <ul class="nav">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">CAT</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">ES</a>
+        </li>
+      </ul>
     </div>
   </div>
