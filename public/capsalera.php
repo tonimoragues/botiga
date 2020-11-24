b174a74e2bcbbfdb162c6591b6ed6f8a4d256499
toni moragues <tmoragues@gmail.com>
2020-11-24 12:38:26 +0100

    5

--- public/capsalera.php
+++ public/capsalera.php
@@ -1,13 +1,17 @@
-  <div class="jumbotron jumbotron-fluid p-3">
-    <div class="container">
-      <h1 class="display-4">Botiga</h1>
-      <ul class="nav">
-        <li class="nav-item">
-          <a class="nav-link active" href="#">CAT</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">ES</a>
-        </li>
-      </ul>
-    </div>
-  </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<a class="navbar-brand" href="#">BOTIGA</a>
+<button class="navbar-toggler" type="<bu></bu>tton" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <ul class="navbar-nav mr-auto">
+    <li class="nav-item active">
+      <a class="nav-link" href="#">CAT</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">ES</a>
+    </li>
+  </ul>
+</div>
+</nav>
