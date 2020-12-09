d22168446651fb99f5a80a7be9192c7fbcb96844
toni moragues <tmoragues@gmail.com>
2020-12-09 13:12:28 +0100

    test rewrite

--- public/inici.php
+++ public/inici.php
@@ -1,5 +1,16 @@
 <?php
 
 $uri=trim($_SERVER['REQUEST_URI'],'/');
-echo $uri;
+$path=explode($uri,'/');
+
+switch ($path[0]) {
+    case "productes":
+        include 'index.php';
+        break;
+    case "carreto":
+        include 'carreto.php';
+        break;
+    default:
+        throw new \Exception('Unexpected value');
+}
 
