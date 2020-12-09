4a99c642bac3ea01bc31f1cf64fcfb660092b80e
toni moragues <tmoragues@gmail.com>
2020-12-09 13:27:21 +0100

    test rewrite

--- public/inici.php
+++ public/inici.php
@@ -1,16 +1,20 @@
 <?php
 
 $uri=trim($_SERVER['REQUEST_URI'],'/');
-$path=explode($uri,'/');
+$path=explode('/',$uri.'/',-1);
 
 switch ($path[0]) {
     case "productes":
-        include 'index.php';
+        if (array_key_exists(1,$path)) {
+            $codi=$path[1];
+            include 'fitxa.php';
+        }
+        else {
+            include 'index.php';
+        }
         break;
     case "carreto":
         include 'carreto.php';
         break;
-    default:
-        throw new \Exception('Unexpected value');
 }
 
