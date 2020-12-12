5529da348f433bd5ff1d75e7cad5334507b58686
toni moragues <tmoragues@gmail.com>
2020-12-12 16:17:24 +0100

    rutes

--- views/fitxa.view.php
+++ views/fitxa.view.php
@@ -17,13 +17,13 @@
         <div class="col-sm"><a href="index.php"><?php echo $traduccions[$lang]['tornar']; ?>  <i class="fas fa-undo"></i></a></div>
     </div>
     <div class="row">
-        <div class="col-sm-6"><img src="/img/<?php echo $codi;?>.jpg" alt="" class="img-fluid"></div>
+        <div class="col-sm-6"><img src="/img/<?= $producte->codi;?>.jpg" alt="" class="img-fluid"></div>
         <div class="col-sm-6">
             <div class="row">
-                <div class="col-sm-11"><h2 style="font-family: 'Julius Sans One', sans-serif;"><?php echo $nom;?></h2></div>
-                <div class="col-sm-11 my-2"><h4><?php echo $preu;?> €</h4></div>
-                <div class="col-sm-11"><a href="carreto.php?producte=<?php echo $codi;?>" class="btn btn-primary"><?php echo $traduccions[$lang]['afegir']; ?><i class="fas fa-shopping-cart"></i></a></div>
-                <div class="col-sm-11 my-3"><p><?php echo $descripcio;?></p></div>
+                <div class="col-sm-11"><h2 style="font-family: 'Julius Sans One', sans-serif;"><?= $producte->nom;?></h2></div>
+                <div class="col-sm-11 my-2"><h4><?= $producte->preu;?> €</h4></div>
+                <div class="col-sm-11"><a href="carreto.php?producte=<?= $producte->codi;?>" class="btn btn-primary"><?php echo $traduccions[$lang]['afegir']; ?><i class="fas fa-shopping-cart"></i></a></div>
+                <div class="col-sm-11 my-3"><p><?= $producte->descripcio;?></p></div>
             </div>
         </div>
     </div>
