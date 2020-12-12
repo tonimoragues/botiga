ce594512cc9810600426cbf29ef7d12f46159f02
toni moragues <tmoragues@gmail.com>
2020-12-12 16:20:59 +0100

    rutes

--- public/fitxa.php
+++ public/fitxa.php
@@ -13,11 +13,10 @@ include "traduccions.php";
 try {
   $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   $stmt = $pdo->prepare("SELECT codi, nom, preu, descripcio FROM productes where codi=$codi");
-  var_dump($codi);
-  die();
   $stmt->execute();
   $producte = $stmt->fetchAll(PDO::FETCH_CLASS, 'Producte');
-
+  var_dump($producte);
+  die();
 } catch (PDOException $e) {
   die($e->getMessage());
 }
